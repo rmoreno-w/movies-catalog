dee4607b762b93ec7bb7d50b3cf5b6ebbaadc703
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-10-02 04:41:36 -0300

    Rotas e forms de Criar e Atualizar Gênero

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,8 +7,39 @@ class GenresController < ApplicationController
   end
 
   def create
+    received_name = params[:genre][:name]
+
+    new_genre = Genre.new(name: received_name)
+
+    if new_genre.save
+      flash[:now] = "Novo gênero cadastrado com sucesso!"
+
+      redirect_to genres_path
+    else
+      render :create
+    end
   end
 
   def show
+    received_id = params[:id]
+    @genre = Genre.find(received_id)
+  end
+
+  def edit
+    received_id = params[:id]
+    @genre = Genre.find(received_id)
+  end
+
+  def update
+    received_id = params[:id]
+    @genre = Genre.find(received_id)
+
+    received_name = params[:genre][:name]
+
+    if @genre.update(name: received_name)
+      redirect_to genre_path(@genre.id)
+    else
+      render :edit
+    end
   end
 end
