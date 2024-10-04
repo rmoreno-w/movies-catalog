a16552f87b866bc5b03005d14acfc5f49c56a79f
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-10-04 02:23:54 -0300

    Linkando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :genres, only: [ :index, :show, :new, :create, :edit, :update ]
   resources :movie_directors, only: [ :index, :show, :new, :create, :edit, :update ]
   resources :movies, only: [ :index, :show, :new, :create, :edit, :update ]
+  root  'home#index'
 end
