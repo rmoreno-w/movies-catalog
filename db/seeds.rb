8f462e0eb17b23e9e61a9415d26958d28d1226bb
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-10-05 19:28:12 -0300

    Criando seeds e ajuste incluindo status de lancamento

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Genre.create([
+  {name: "Comédia"},
+  {name: "Ação"},
+  {name: "Policial"}])
+
+  
+MovieDirector.create!([
+  {name: "Astrogildo Ramos Fonseca", nationality: "Brasileiro", birth_date: "1990-05-02", genre_id: Genre.first.id},
+  {name: "Broncos Silva Oliveira", nationality: "Brasileiro", birth_date: "1960-07-05", genre_id: Genre.last.id}])
+    
+# puts "Created Directors"
+Movie.create!([
+  {
+    title: "Poeira em Alto Mar",
+    year: 1990, synopsis: "Um filme realmente inesperado",
+    origin_country: "Sérvia",
+    movie_length: 150,
+    movie_director_id: MovieDirector.first.id,
+    genre_id: Genre.last.id,
+    draft_status: 0,
+    releasing_status: 1
+  },
+  {
+    title: "A volta dos que não foram",
+    year: 1990, synopsis: "Um filme chocante",
+    origin_country: "Prússia",
+    movie_length: 200,
+    movie_director_id: MovieDirector.first.id,
+    genre_id: Genre.last.id,
+    draft_status: 0
+  },
+  {
+    title: "A volta dos que ainda não foram mas estão para ir",
+    year: 1990, synopsis: "Um filme chocante e inesperado",
+    origin_country: "Prússia",
+    movie_length: 200,
+    movie_director_id: MovieDirector.last.id,
+    genre_id: Genre.first.id,
+    draft_status: 1}])
